e7e73fa479a10a4a2514ebf6b7f982c8f401803c
martinez237 <martinezofficiel526@gmail.com>
2026-03-31 14:09:48 +0100

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+
+    ],
+
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+export default config
